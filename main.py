cc60acf035914b94d41f90f43f96ca5f18472562
Piotr Brudny <pbrudny@gmail.com>
2024-06-11 14:39:55 +0200

    Import the Library in main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# 
+from library import Library
+# I want to create a library and display its name
+
+our_library = Library('Wseiz Library', 'Rejtana 17, Warsaw')
+
+print("The name: ", our_library.name)
